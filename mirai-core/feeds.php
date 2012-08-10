4659c4177c0185fc58259a6fde93d66d9e2fb016
Ryan Rampersad <ryan.rampersad@gmail.com>
2012-08-10 05:35:29 -0500

    Put linked list into its own class

--- mirai-core/feeds.php
+++ mirai-core/feeds.php
@@ -1,21 +1,30 @@
 <?php
-add_filter('posts_where', 'll_feed_delay');
-function ll_feed_delay($where) {
-  global $wpdb;
-  
-  $do_delay = true;
-  if (isset($_GET['nodelay'])) $do_delay = false; 
 
-  if (is_feed() && $do_delay) {
-    $now = gmdate('Y-m-d H:i:s');
-    $wait = '15';
-    $device = 'MINUTE';
-    $where .= " AND TIMESTAMPDIFF($device, $wpdb->posts.post_date_gmt, '$now') > $wait ";
-  }
-  
-  return $where;
-}
+class Mirai_Feeds {
 
-function ll_remove_comments_rss( $for_comments ) {
-    return;
+	public static function initialize() {
+		add_action('posts_where', 'Mirai_Feeds::feed_delay');
+		add_filter('post_comments_feed_link', 'Mirai_Feeds::remove_comments');
+	}
+
+	public static function feed_delay($where) {
+	  global $wpdb;
+	  
+	  $do_delay = true;
+	  if (isset($_GET['nodelay'])) $do_delay = false; 
+
+	  if (is_feed() && $do_delay) {
+	    $now = gmdate('Y-m-d H:i:s');
+	    $wait = '15';
+	    $device = 'MINUTE';
+	    $where .= " AND TIMESTAMPDIFF($device, $wpdb->posts.post_date_gmt, '$now') > $wait ";
+	  }
+	  
+	  return $where;
+	}
+
+	public static function remove_comments($for_comments) {
+		return;
+	}
+
 }
--- mirai-core/linked-list.php
+++ mirai-core/linked-list.php
@@ -4,164 +4,162 @@
 	Linked list and extended functionality.
 */
 
-add_action('add_meta_boxes', 'll_add_meta_box');
-add_action('save_post', 'll_save', 10, 2);
-add_action('wp_before_admin_bar_render', 'll_admin_bar');
-add_action('load-post-new.php', '_ll_format_action');
+class Mirai_LinkedList {
 
-function _ll_format_action() {
-		if ( isset($_GET['format']) && !empty($_GET['format']) ) {
-			if ('link' == $_GET['format']) add_action('admin_footer', 'mirai_linked_list_preselect_script');
+	public static function initialize() {
+		self::attach_actions();
+		self::attach_filters();
 	}
-}
 
-add_action('admin_footer', 'mirai_linked_list_autoselect_script');
+	public static function attach_actions() {
+		add_action('add_meta_boxes', 'Mirai_LinkedList::add_meta_box');
+		add_action('save_post', 'Mirai_LinkedList::save_box', 10, 2);
+		add_action('wp_before_admin_bar_render', 'Mirai_LinkedList::modify_admin_bar');
+		add_action('load-post-new.php', 'Mirai_LinkedList::new_link');
+		add_action('admin_footer', 'Mirai_LinkedList::script_autoselect');
+	}
 
+	public static function attach_filters() {
+		add_filter('the_content', 'Mirai_LinkedList::feed_permalink');
+		add_filter('the_excerpt_rss', 'Mirai_LinkedList::feed_permalink');
+		add_action('the_permalink_rss', 'Mirai_LinkedList::feed_title_url');
+		add_filter('the_title_rss', 'Mirai_LinkedList::feed_symbol');
+	}
 
-function _mirai_get_links_category() {
-	$category = get_category_by_slug('links');
-	$category_id = $category->term_id;
-	return $category_id;
-}
+	public static function add_meta_box() {
+		add_meta_box('mirai_linked_list', 'Linked List URL', 'Mirai_LinkedList::display_box', 'post');
+	}
 
+	public static function new_link() {
+		if ( Mirai_LinkedList::admin_is_link_format() ) add_action('admin_footer', 'Mirai_LinkedList::script_preselect');
+	}
 
+	public static function admin_is_link_format() {
+		return (isset($_GET['format']) && !empty($_GET['format']) && ('link' == $_GET['format']));
+	}
 
-function mirai_linked_list_preselect_script() {
-?>
+	public static function display_box($object, $box) {
+		wp_nonce_field( basename(__FILE__), 'll_nonce' );
 
-<script type="text/javascript">
-	jQuery(document).ready(function($){
-		$('#post-format-link').attr('checked', true);
-		$('#in-category-<?php echo _mirai_get_links_category(); ?>').attr('checked', true);
-	});
-</script>
+		echo '<p>';
+		echo '<label for="ll_url">';
+		_e('Add a URL to replace the title-permalink for this post in the <abbr>Linked List</abbr>.');
+		echo '</label>';
+		echo '<input type="text" id="ll_url" name="ll_url" value="' . esc_attr( get_post_meta($object->ID, 'll_url', true) ) . '" size="30" class="widefat" autocomplete="off" />';
+		echo '</p>';
+	}
 
-<?php
-}
+	public static function save_box($post_id, $post) {
+		if ( !isset($_POST['ll_nonce']) || !wp_verify_nonce($_POST['ll_nonce'], basename(__FILE__)) ) {
+			return $post_id;
+		}
 
-function mirai_linked_list_autoselect_script() {
-?>
+		$post_type = get_post_type_object($post->post_type);
+		if ( !current_user_can($post_type->cap->edit_post, $post_id) ) {
+			return $post_id;
+		}
 
-<script type="text/javascript">
-	jQuery(document).ready(function($){
-		$('#ll_url').blur(function(){
-			$('#post-format-link').attr('checked', true);
-			$('#in-category-<?php echo _mirai_get_links_category(); ?>').attr('checked', true);
-		});
-	});
-</script>
+		$new = (isset($_POST['ll_url'])) ? sanitize_text_field($_POST['ll_url']) : '';
 
-<?php
-}
+		$meta_key = 'll_url';
 
-function ll_admin_bar() {
-	global $wp_admin_bar;
-	$wp_admin_bar->add_menu(array(
-		'title' => 'Link',
-		'href' => admin_url('post-new.php?format=link'),
-		'parent' => 'new-content',
-		'id' => 'mirai_linked_list'
+		$meta_value = get_post_meta($post_id, $meta_key, true);
 
-	));
-}
-
-function ll_url_rewrite($content) {
-	if ( 'link' == get_post_format() ) {
-		$url = mirai_get_ll_url();
-		if ( $url != false ) {
-			echo '<![CDATA[' . $url . ']]>';
-			return $url;
+		if ( $new && '' == $meta_value ) {
+			add_post_meta($post_id, $meta_key, $new, true);
+		} elseif ($new && $new != $meta_value) {
+			update_post_meta($post_id, $meta_key, $new);
+		} elseif ('' == $new && $meta_value) {
+			delete_post_meta($post_id, $meta_key, $meta_value);
 		}
 	}
-	echo $content;
-	return $content;
-}
 
-function ll_permalink_insert($content) {
-	$symbol = '&#8733;';
-	if ( 'link' == get_post_format() && is_feed() ) {
-		$content = $content . '<div><p style="font-size: 2em;"><a href="'.get_permalink().'">&#8733;</a></p></div>';
-	} 
-	return $content;
-}
+	public static function get_linked_list_url() {
+		$id = get_the_ID();
+		if ( empty($id) ) return false;
 
-function ll_symbol_insert($content) {
-	$symbol = '&#8733;';
-	if ( 'link' == get_post_format() && is_feed() ) {
-		//$content = '<![CDATA[&rarr;  ' . $content . ']]>';
-		$content = '→  ' . $content . '';
-	} 
-	return $content;
-}
+		$ll_url = get_post_meta($id, 'll_url', true);
+		
+		$ll_url = apply_filters('pre_ll_url', $ll_url);
 
-add_filter('the_content', 'll_permalink_insert');
-add_filter('the_excerpt_rss', 'll_permalink_insert');
-add_action('the_permalink_rss', 'll_url_rewrite');
-add_filter('the_title_rss', 'll_symbol_insert');
+		if ( empty($ll_url) ) return false;
 
-
-
-add_filter('post_comments_feed_link', 'll_remove_comments_rss');
-
-
-function ll_add_meta_box() {
-	add_meta_box('mirai_linked_list', 'Linked List URL', 'll_box', 'post');
-}
-
-function ll_box($object, $box) {
-
-	wp_nonce_field( basename(__FILE__), 'll_nonce' );
-
-	echo '<p>';
-	echo '<label for="ll_url">';
-	_e('Add a URL to replace the title-permalink for this post in the <abbr>Linked List</abbr>.');
-	echo '</label>';
-	echo '<input type="text" id="ll_url" name="ll_url" value="' . esc_attr( get_post_meta($object->ID, 'll_url', true) ) . '" size="30" class="widefat" autocomplete="off" />';
-	echo '</p>';
-
-}
-
-
-
-function ll_save($post_id, $post) {
-
-	if ( !isset($_POST['ll_nonce']) || !wp_verify_nonce($_POST['ll_nonce'], basename(__FILE__)) ) {
-		return $post_id;
+		return $ll_url;
 	}
 
-	$post_type = get_post_type_object($post->post_type);
-	if ( !current_user_can($post_type->cap->edit_post, $post_id) ) {
-		return $post_id;
+	public static function modify_admin_bar() {
+		global $wp_admin_bar;
+		$wp_admin_bar->add_menu(array(
+			'title' => 'Link',
+			'href' => admin_url('post-new.php?format=link'),
+			'parent' => 'new-content',
+			'id' => 'Mirai_LinkedList_linked_list'
+
+		));
 	}
 
-	$new = (isset($_POST['ll_url'])) ? sanitize_text_field($_POST['ll_url']) : '';
+	public static function feed_title_url($content) {
+		if ( 'link' == get_post_format() ) {
+			$url = Mirai_LinkedList_get_ll_url();
+			if ( $url != false ) {
+				echo '<![CDATA[' . $url . ']]>';
+				return $url;
+			}
+		}
+		echo $content;
+		return $content;
+	}
 
-	$meta_key = 'll_url';
+	public static function feed_permalink($content) {
+		$symbol = '&#8733;';
+		if ( 'link' == get_post_format() && is_feed() ) {
+			$content = $content . '<div><p style="font-size: 2em;"><a href="'.get_permalink().'">&#8733;</a></p></div>';
+		} 
+		return $content;
+	}
 
-	$meta_value = get_post_meta($post_id, $meta_key, true);
+	public static function feed_symbol($content) {
+		$symbol = '&#8733;';
+		if ( 'link' == get_post_format() && is_feed() ) {
+			//$content = '<![CDATA[&rarr;  ' . $content . ']]>';
+			$content = '→  ' . $content . '';
+		} 
+		return $content;
+	}
 
-	if ( $new && '' == $meta_value ) {
-		add_post_meta($post_id, $meta_key, $new, true);
-	} elseif ($new && $new != $meta_value) {
-		update_post_meta($post_id, $meta_key, $new);
-	} elseif ('' == $new && $meta_value) {
-		delete_post_meta($post_id, $meta_key, $meta_value);
+	public static function script_autoselect() {
+		?>
+		<script type="text/javascript">
+			jQuery(document).ready(function($){
+				$('#ll_url').blur(function(){
+					$('#post-format-link').attr('checked', true);
+					$('#in-category-<?php echo Mirai_LinkedList::get_links_category(); ?>').attr('checked', true);
+				});
+			});
+		</script>
+		<?php
+	}
+
+	public static function script_preselect() {
+		?>
+		<script type="text/javascript">
+			jQuery(document).ready(function($){
+				$('#ll_url').blur(function(){
+					$('#post-format-link').attr('checked', true);
+					$('#in-category-<?php echo Mirai_LinkedList::get_links_category(); ?>').attr('checked', true);
+				});
+			});
+		</script>
+		<?php
+	}
+
+	public static function get_links_category() {
+		$category = get_category_by_slug('links');
+		$category_id = $category->term_id;
+		return $category_id;
 	}
 
 }
 
-function mirai_get_ll_url() {
-
-	$id = get_the_ID();
-	if ( empty($id) ) return false;
-
-	$ll_url = get_post_meta($id, 'll_url', true);
-	
-	$ll_url = apply_filters('pre_ll_url', $ll_url);
-
-	if ( empty($ll_url) ) return false;
-
-	return $ll_url;
-}
 
 ?>
